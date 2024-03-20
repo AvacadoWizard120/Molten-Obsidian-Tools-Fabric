f0e23c0485966b2792db585f8e2072e15c3e11a8
ConfusingFool93 <edenjudeurias@gmail.com>
2024-03-20 00:29:06 -0500

    Init 1.19.3 Fabric

---
+++ src/main/java/confusingfool93/moltenobsidiantools/MoltenObsidianToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package confusingfool93.moltenobsidiantools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoltenObsidianToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
