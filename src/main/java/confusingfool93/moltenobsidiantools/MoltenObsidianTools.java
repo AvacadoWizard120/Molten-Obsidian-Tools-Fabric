5a4289d262ce99ed1160cae283d0d534d151ec98
ConfusingFool93 <edenjudeurias@gmail.com>
2024-03-20 23:51:14 -0500

    Added the first item and the texture is all working the next step is to add the custom creative mode tab!

--- src/main/java/confusingfool93/moltenobsidiantools/MoltenObsidianTools.java
+++ src/main/java/confusingfool93/moltenobsidiantools/MoltenObsidianTools.java
@@ -1,5 +1,6 @@
 package confusingfool93.moltenobsidiantools;
 
+import confusingfool93.moltenobsidiantools.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,11 +12,8 @@ public class MoltenObsidianTools implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitialize()
+	{
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/confusingfool93/moltenobsidiantools/item/ModItems.java
@@ -0,0 +1,40 @@
+package confusingfool93.moltenobsidiantools.item;
+
+import confusingfool93.moltenobsidiantools.MoltenObsidianTools;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class ModItems
+{
+    public static final Item MOLTEN_OBSIDIAN = registerItem("molten_obsidian",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registries.ITEM, new Identifier(MoltenObsidianTools.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup()
+    {
+        addToItemGroup(ItemGroups.INGREDIENTS, MOLTEN_OBSIDIAN);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item)
+    {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
+    public static void registerModItems()
+    {
+        MoltenObsidianTools.LOGGER.info("Registering the Items for " + MoltenObsidianTools.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
