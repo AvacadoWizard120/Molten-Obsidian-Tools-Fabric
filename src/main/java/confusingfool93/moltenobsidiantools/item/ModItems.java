cf859c598d8ead28b565ef3865e349d569d53070
ConfusingFool93 <edenjudeurias@gmail.com>
2024-03-21 14:36:44 -0500

    Added everything as far as I'm concerned this is the entire mod ported over to Fabric!

--- src/main/java/confusingfool93/moltenobsidiantools/item/ModItems.java
+++ src/main/java/confusingfool93/moltenobsidiantools/item/ModItems.java
@@ -3,9 +3,7 @@ package confusingfool93.moltenobsidiantools.item;
 import confusingfool93.moltenobsidiantools.MoltenObsidianTools;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -19,6 +17,30 @@ public class ModItems
     public static final Item REINFORCED_MOLTEN_OBSIDIAN = registerItem("reinforced_molten_obsidian",
             new Item(new FabricItemSettings().maxCount(1)));
 
+    public static final Item MOLTEN_OBSIDIAN_PICKAXE = registerItem("molten_obsidian_pickaxe",
+            new PickaxeItem(ModToolMaterial.MOlTEN, 4, -1f, new FabricItemSettings()));
+
+    public static final Item MOLTEN_OBSIDIAN_AXE = registerItem("molten_obsidian_axe",
+            new AxeItem(ModToolMaterial.MOlTEN, 10, -1f, new FabricItemSettings()));
+
+    public static final Item MOLTEN_OBSIDIAN_SHOVEL = registerItem("molten_obsidian_shovel",
+            new ShovelItem(ModToolMaterial.MOlTEN, 3, -1f, new FabricItemSettings()));
+
+    public static final Item MOLTEN_OBSIDIAN_HOE = registerItem("molten_obsidian_hoe",
+            new HoeItem(ModToolMaterial.MOlTEN, 2, -1f, new FabricItemSettings()));
+
+    public static final Item REINFORCED_OBSIDIAN_PICKAXE = registerItem("reinforced_obsidian_pickaxe",
+            new PickaxeItem(ModToolMaterial.REINFORCED, 4, -1f, new FabricItemSettings()));
+
+    public static final Item REINFORCED_OBSIDIAN_HOE = registerItem("reinforced_obsidian_hoe",
+            new HoeItem(ModToolMaterial.REINFORCED, 2, -1f, new FabricItemSettings()));
+
+    public static final Item REINFORCED_OBSIDIAN_SHOVEL = registerItem("reinforced_obsidian_shovel",
+            new ShovelItem(ModToolMaterial.REINFORCED, 3, -1f, new FabricItemSettings()));
+
+    public static final Item REINFORCED_OBSIDIAN_AXE = registerItem("reinforced_obsidian_axe",
+            new AxeItem(ModToolMaterial.REINFORCED, 10, -1f, new FabricItemSettings()));
+
     private static Item registerItem(String name, Item item)
     {
         return Registry.register(Registries.ITEM, new Identifier(MoltenObsidianTools.MOD_ID, name), item);
@@ -28,9 +50,25 @@ public class ModItems
     {
         addToItemGroup(ItemGroups.INGREDIENTS, MOLTEN_OBSIDIAN);
         addToItemGroup(ItemGroups.INGREDIENTS, REINFORCED_MOLTEN_OBSIDIAN);
+        addToItemGroup(ItemGroups.TOOLS, MOLTEN_OBSIDIAN_AXE);
+        addToItemGroup(ItemGroups.TOOLS, MOLTEN_OBSIDIAN_HOE);
+        addToItemGroup(ItemGroups.TOOLS, MOLTEN_OBSIDIAN_SHOVEL);
+        addToItemGroup(ItemGroups.TOOLS, MOLTEN_OBSIDIAN_PICKAXE);
+        addToItemGroup(ItemGroups.TOOLS, REINFORCED_OBSIDIAN_AXE);
+        addToItemGroup(ItemGroups.TOOLS, REINFORCED_OBSIDIAN_HOE);
+        addToItemGroup(ItemGroups.TOOLS, REINFORCED_OBSIDIAN_PICKAXE);
+        addToItemGroup(ItemGroups.TOOLS, REINFORCED_OBSIDIAN_SHOVEL);
 
         addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, MOLTEN_OBSIDIAN);
         addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, REINFORCED_MOLTEN_OBSIDIAN);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, MOLTEN_OBSIDIAN_AXE);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, MOLTEN_OBSIDIAN_PICKAXE);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, MOLTEN_OBSIDIAN_HOE);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, MOLTEN_OBSIDIAN_SHOVEL);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, REINFORCED_OBSIDIAN_SHOVEL);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, REINFORCED_OBSIDIAN_PICKAXE);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, REINFORCED_OBSIDIAN_HOE);
+        addToItemGroup(ModItemGroup.MOLDTEN_OBSIDIAN_TOOLS, REINFORCED_OBSIDIAN_AXE);
     }
 
     private static void addToItemGroup(ItemGroup group, Item item)
---
+++ src/main/java/confusingfool93/moltenobsidiantools/item/ModToolMaterial.java
@@ -0,0 +1,62 @@
+package confusingfool93.moltenobsidiantools.item;
+
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterial implements ToolMaterial
+{
+    MOlTEN(MiningLevels.DIAMOND, 1900, 9.4f, 3.5f, 3,
+            () -> Ingredient.ofItems(ModItems.MOLTEN_OBSIDIAN)),
+    REINFORCED(MiningLevels.NETHERITE, 3000, 20f, 4.0f, 20,
+            () -> Ingredient.ofItems(ModItems.MOLTEN_OBSIDIAN))
+    ;
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    ModToolMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
