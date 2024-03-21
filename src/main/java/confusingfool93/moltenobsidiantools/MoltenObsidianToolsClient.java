7160a25acd990f7469eccb229cc0097cad2966d3
ConfusingFool93 <edenjudeurias@gmail.com>
2024-03-20 23:06:06 -0500

    Got some basic things set up.

---
+++ src/main/java/confusingfool93/moltenobsidiantools/MoltenObsidianToolsClient.java
@@ -0,0 +1,12 @@
+package confusingfool93.moltenobsidiantools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoltenObsidianToolsClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
